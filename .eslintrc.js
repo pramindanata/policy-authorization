f2ba1af7191943ffa93b96d4aa4171de335d2b33
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-04-14 14:26:24 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-console': 'warn',
+  },
+};
---
+++ test/demo.test.ts
@@ -0,0 +1,5 @@
+describe('Demo', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true);
+  });
+});
